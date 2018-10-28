e78c147b30afdeae1e4dd84eb7f080c72906d3f1
Dan Andersson <760686+daon@users.noreply.github.com>
2018-10-28 21:10:53 +0100

    add solution

--- BowlingGame.Tests/GameTest.cs
+++ BowlingGame.Tests/GameTest.cs
@@ -15,12 +15,52 @@ namespace BowlingGame.Tests
     [Fact]
     public void All_rolls_in_the_gutter()
     {
-      for (int i = 0; i < 20; i++)
-      {
-        game.Roll(0);
-      }
-
+      ManyRolls(20, 0);
       Assert.Equal(game.GetScore(), 0);
+    }
+
+    [Fact]
+    public void All_rolls_hit_one_pin() {
+      ManyRolls(20, 1);
+      Assert.Equal(game.GetScore(), 20);
+    }
+
+    [Fact]
+    public void Rolls_one_spare() {
+      RollSpare();
+      game.Roll(3);
+      ManyRolls(17, 0);
+      Assert.Equal(game.GetScore(), 16);
+    }
+
+    [Fact]
+    public void Rolls_one_strike() {
+      RollStrike();
+      game.Roll(4);
+      game.Roll(3);
+      ManyRolls(16, 0);
+      Assert.Equal(game.GetScore(), 24);
+    }
+
+    [Fact]
+    public void Rolls_perfect_game() {
+      ManyRolls(20, 10);
+      Assert.Equal(game.GetScore(), 300);
+    }
+
+    private void ManyRolls(int rolls, int pins) {
+      for(int i = 0; i < rolls; i++) {
+        game.Roll(pins);
+      }
+    }
+
+    private void RollSpare() {
+      game.Roll(5);
+      game.Roll(5);
+    }
+
+    private void RollStrike() {
+      game.Roll(10);
     }
   }
 }
--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -4,14 +4,62 @@ namespace BowlingGame
 {
   public class Game
   {
+    private readonly int[] rolls = new int[21];
+    private int currentRoll = 0;
+
     public void Roll(int pins)
     {
-      throw new NotImplementedException();
+      this.rolls[this.currentRoll++] = pins;
     }
 
     public int GetScore()
     {
-      throw new NotImplementedException();
+      int score = 0;
+      int frameIndex = 0;
+      for (int frame = 0; frame < 10; frame++)
+      {
+        if (this.isStrike(frameIndex))
+        {
+          score += 10 + this.strikeBonus(frameIndex);
+          frameIndex++;
+        }
+        else if (this.isSpare(frameIndex))
+        {
+          score += 10 + this.spareBonus(frameIndex);
+          frameIndex += 2;
+        }
+        else
+        {
+          score += this.sumOfPinsInFrame(frameIndex);
+          frameIndex += 2;
+        }
+      }
+      return score;
+    }
+
+    private int sumOfPinsInFrame(int frameIndex)
+    {
+      return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+    }
+
+    private int strikeBonus(int frameIndex)
+    {
+      return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+    }
+
+    private int spareBonus(int frameIndex)
+    {
+      return this.rolls[frameIndex + 2];
+    }
+
+    private bool isStrike(int frameIndex)
+    {
+      return this.rolls[frameIndex] == 10;
+    }
+
+    private bool isSpare(int frameIndex)
+    {
+      return this.rolls[frameIndex] + this.rolls[frameIndex + 1] == 10;
     }
   }
 }
